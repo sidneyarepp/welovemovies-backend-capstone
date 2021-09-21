536202da2babfb0df6abb666a91d78d8d064f363
sidneyarepp <sidneyarepp@outlook.com>
2021-09-21 17:02:23 -0400

    Correcting routes to only include movies, reviews, and theaters.

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ const app = express();
 const moviesRouter = require('./movies/movies.router');
 const reviewsRouter = require('./reviews/reviews.router');
 const theatersRouter = require('./theaters/theaters.router');
-const movieTheatersRouter = require('./movies_theaters/movies_theaters.router');
-const criticsRouter = require('./critics/critics.router');
+
 
 app.use(express.json());
 
@@ -17,12 +16,6 @@ app.use('/reviews', reviewsRouter);
 
 //theaters route handler
 app.use('/theaters', theatersRouter);
-
-//movie_theaters route handler
-app.use('/movies-theaters', movieTheatersRouter);
-
-//critics route handler
-app.use('/critics', criticsRouter);
 
 //Invalid found handler
 app.use((req, res, next) => {
